cf9e533587dbce88927760872b4416acc72486af
Elisa Camillo <ecamillo@mppr.mp.br>
2025-09-15 19:10:23 -0300

    CategoriaController e endpoints de listagem e cadastro de categorias

--- src/main/java/br/com/alura/comex/db/ConnectionFactory.java
+++ src/main/java/br/com/alura/comex/db/ConnectionFactory.java
@@ -7,15 +7,15 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     public Connection criaConexao() {
-        String usuario = "sa";
-        String senha = "";
+        String usuario = "admin";
+        String senha = "admin";
 
         try {
-            String url = "jdbc:h2:./comex;;DB_CLOSE_ON_EXIT=FALSE;"
-                    //   + "INIT=runscript from './schema.sql'\\;"
-                    //   + "runscript from './data.sql'"
-                    ;
-            
+            String url = "jdbc:postgresql://localhost:5432/comex"
+            // + "INIT=runscript from './schema.sql'\\;"
+            // + "runscript from './data.sql'"
+            ;
+
             return DriverManager.getConnection(url, usuario, senha);
         } catch (SQLException e) {
             e.printStackTrace();
