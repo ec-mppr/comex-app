b035fe361c6cd3b0c45fc59603176bbb3744c3c8
Elisa Camillo <ecamillo@mppr.mp.br>
2025-09-24 12:55:29 -0300

    filtrando requisições

--- src/main/java/br/com/alura/comex/security/SecurityFilter.java
+++ src/main/java/br/com/alura/comex/security/SecurityFilter.java
@@ -1,5 +1,52 @@
 package br.com.alura.comex.security;
 
-public class SecurityFilter {
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.alura.comex.usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private TokenService tokenService;
+
+  @Autowired
+  private UsuarioRepository usuarioRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    var tokenJWT = getToken(request);
+
+    if (tokenJWT != null) {
+      System.out.println(tokenJWT);
+      var subject = tokenService.getSubject(tokenJWT);
+      var usuario = usuarioRepository.findByEmail(subject);
+
+      var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private String getToken(HttpServletRequest request) {
+    var authorizationHeader = request.getHeader("Authorization");
+    if (authorizationHeader == null) {
+      return null;
+    }
+
+    return authorizationHeader.replace("Bearer ", "");
+  }
 
 }
--- src/main/java/br/com/alura/comex/security/TokenService.java
+++ src/main/java/br/com/alura/comex/security/TokenService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import br.com.alura.comex.usuario.Usuario;
 
@@ -24,6 +25,19 @@ public class TokenService {
           .sign(algoritmo);
     } catch (JWTCreationException exception) {
       throw new RuntimeException("Erro ao gerar token JWT: ", exception);
+    }
+  }
+
+  public String getSubject(String tokenJWT) {
+    try {
+      var algoritmo = Algorithm.HMAC256("12345678");
+      return JWT.require(algoritmo)
+          .withIssuer("Comex App")
+          .build()
+          .verify(tokenJWT)
+          .getSubject();
+    } catch (JWTVerificationException exception) {
+      throw new RuntimeException("Token JWT inválido ou expirado!");
     }
   }
 
